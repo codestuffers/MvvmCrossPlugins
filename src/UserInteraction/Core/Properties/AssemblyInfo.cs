3450c3a23a8778f45adb4391c89a02ad45b0e28c
Andrew Benz <andorbal@gmail.com>
2014-02-23 21:18:21 -0600

    Update assembly properties

--- src/UserInteraction/Core/Properties/AssemblyInfo.cs
+++ src/UserInteraction/Core/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("codestuffers.MvvmCross.UserInteraction.Core")]
+[assembly: AssemblyTitle("codestuffers.MvvmCrossPlugins.UserInteraction.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("codestuffers.MvvmCross.UserInteraction.Core")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("codestuffers, LLC")]
+[assembly: AssemblyProduct("codestuffers.MvvmCrossPlugins.UserInteraction.Core")]
+[assembly: AssemblyCopyright("Copyright ©codestuffers, LLC  2014")]
+[assembly: AssemblyTrademark("codestuffers, LLC")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
--- src/UserInteraction/Examples/Examples.Droid/Properties/AssemblyInfo.cs
+++ src/UserInteraction/Examples/Examples.Droid/Properties/AssemblyInfo.cs
@@ -4,20 +4,24 @@ using Android.App;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle ("Examples.Droid")]
-[assembly: AssemblyDescription ("")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("Andy")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion ("1.0.0")]
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyTitle("codestuffers.MvvmCrossPlugins.UserInteraction.Examples.Droid")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("codestuffers, LLC")]
+[assembly: AssemblyProduct("codestuffers.MvvmCrossPlugins.UserInteraction.Examples.Droid")]
+[assembly: AssemblyCopyright("Copyright ©codestuffers, LLC  2014")]
+[assembly: AssemblyTrademark("codestuffers, LLC")]
+[assembly: AssemblyCulture("")]
 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
