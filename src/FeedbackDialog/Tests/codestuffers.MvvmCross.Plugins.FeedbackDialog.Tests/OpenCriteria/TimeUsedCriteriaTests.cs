ae265ccefa241356efe292aabb8c771e1dfd1983
Andrew Benz <andorbal@gmail.com>
2014-03-13 19:01:49 -0500

    Add TimeUsedCriteria for opening the feedback dialog

---
+++ src/FeedbackDialog/Tests/codestuffers.MvvmCross.Plugins.FeedbackDialog.Tests/OpenCriteria/TimeUsedCriteriaTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using codestuffers.MvvmCross.Plugins.FeedbackDialog.OpenCriteria;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace codestuffers.MvvmCross.Plugins.FeedbackDialog.Tests.OpenCriteria
+{
+    [TestFixture()]
+    public class TimeUsedCriteriaTests
+    {
+        [Test]
+        public void ShouldOpen_ReturnsTrue_WhenUsedLongerThanRequiredTime()
+        {
+            var testObject = new TimeUsedCriteria(TimeSpan.FromDays(3))
+            {
+                CurrentTime = () => new DateTime(2014, 5, 5, 12, 0, 0)
+            };
+
+            testObject.ShouldOpen(new FeedbackData {AppInstallDate = new DateTime(2014, 5, 2, 11, 59, 59)})
+                .Should()
+                .BeTrue();
+        }
+
+        [Test]
+        public void ShouldOpen_ReturnsFalse_WhenUsedShorterThanRequiredTime()
+        {
+            var testObject = new TimeUsedCriteria(TimeSpan.FromDays(3))
+            {
+                CurrentTime = () => new DateTime(2014, 5, 5, 12, 0, 0)
+            };
+
+            testObject.ShouldOpen(new FeedbackData { AppInstallDate = new DateTime(2014, 5, 2, 12, 0, 1) })
+                .Should()
+                .BeFalse();
+        }
+
+        [Test]
+        public void CurrentTime_ReturnsActualTime_WhenNotExplicitlySet()
+        {
+            var lowTime = DateTime.UtcNow;
+            var testObject = new TimeUsedCriteria(TimeSpan.FromDays(1));
+            testObject.CurrentTime() .Should().BeWithin(TimeSpan.FromSeconds(2)).After(lowTime);
+        }
+    }
+}
---
+++ src/FeedbackDialog/codestuffers.MvvmCross.Plugins.FeedbackDialog/OpenCriteria/TimeUsedCriteria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace codestuffers.MvvmCross.Plugins.FeedbackDialog.OpenCriteria
+{
+    internal class TimeUsedCriteria : IOpenDialogCriteria
+    {
+        private readonly TimeSpan _fromDays;
+
+        public TimeUsedCriteria(TimeSpan fromDays)
+        {
+            _fromDays = fromDays;
+            CurrentTime = () => DateTime.UtcNow;
+        }
+
+        public Func<DateTime> CurrentTime { get; set; }
+
+        public bool ShouldOpen(FeedbackData data)
+        {
+            return CurrentTime().Subtract(_fromDays) > data.AppInstallDate;
+        }
+    }
+}
